3d9f3251d07b49031159f9aeabce5f5064a5f11a
Morwenn <morwenn29@hotmail.fr>
2017-04-18 22:11:28 +0200

    Remove now useless include of utility/detection.h

--- include/cpp-sort/adapters/small_array_adapter.h
+++ include/cpp-sort/adapters/small_array_adapter.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2016 Morwenn
+ * Copyright (c) 2015-2017 Morwenn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,7 +32,6 @@
 #include <type_traits>
 #include <utility>
 #include <cpp-sort/sorter_traits.h>
-#include <cpp-sort/utility/detection.h>
 #include <cpp-sort/utility/is_in_pack.h>
 
 namespace cppsort
