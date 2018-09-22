7159901bd910be096440b7149e4731764309c41d
Morwenn <morwenn29@hotmail.fr>
2018-09-22 18:36:46 +0200

    std::array<T, 0> works with Clang 7, not Clang 6

--- include/cpp-sort/utility/buffer.h
+++ include/cpp-sort/utility/buffer.h
@@ -107,7 +107,7 @@ namespace cppsort::utility
         };
     };
 
-#if defined(_LIBCPP_VERSION) && _LIBCPP_VERSION < 6000
+#if defined(_LIBCPP_VERSION) && _LIBCPP_VERSION < 7000
     template<>
     struct fixed_buffer<0>
     {
